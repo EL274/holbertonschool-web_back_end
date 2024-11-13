a99c8640049d846f7c2ef66a692801ea868a9b61
EL274 <tresormakosso510@gmail.com>
2024-11-13 10:23:27 +0100

    all

---
+++ 11-createEmployeesObject.js
@@ -0,0 +1,12 @@
+export default function createEmployeesObject(departmentName, employees) {
+    // Utilisation des noms de propriétés calculés (computed property names)
+    return {
+      [departmentName]: employees
+    };
+  }
+  export default function createEmployeesObject(departmentName, employees) {
+    // Utilisation des noms de propriétés calculés (computed property names)
+    return {
+      [departmentName]: employees
+    };
+  }
