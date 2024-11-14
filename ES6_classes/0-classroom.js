0c192870c95382b362957f1ce82e7cb0c176a85b
EL274 <tresormakosso510@gmail.com>
2024-11-14 07:38:11 +0100

    ok

---
+++ ES6_classes/0-classroom.js
@@ -0,0 +1,11 @@
+public class ClassRoom {
+    private int _maxStudentsSize;
+
+    public ClassRoom(int maxStudentsSize) {
+        this._maxStudentsSize = maxStudentsSize;
+    }
+
+    public int get_maxStudentsSize() {
+        return _maxStudentsSize;
+    }
+}
