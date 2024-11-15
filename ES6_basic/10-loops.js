9b7950c5b1ee6f2e31b778ad046b6b13f7cd9b7f
EL274 <tresormakosso510@gmail.com>
2024-11-15 16:49:11 +0100

    ok

--- ES6_basic/10-loops.js
+++ ES6_basic/10-loops.js
@@ -1,10 +1,8 @@
 export default function appendToEachArrayValue(array, appendString) {
-    // Utilisation de 'for...of' pour itérer directement sur les valeurs du tableau
-    for (let value of array) {
-      // Mise à jour de la valeur dans le tableau
-      // L'index de 'value' dans 'array' est trouvé avec indexOf
-      array[array.indexOf(value)] = appendString + value;
-    }
-  
-    return array;
+  const newArray = [];
+  for (const value of array) {
+    newArray.push(appendString + value);
   }
+
+  return newArray;
+}
