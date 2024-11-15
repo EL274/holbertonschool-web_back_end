77b53c6bf099ae6bb675108f537fce2316d9491f
EL274 <tresormakosso510@gmail.com>
2024-11-15 16:38:34 +0100

    ok

--- ES6_basic/9-getFullBudget.js
+++ ES6_basic/9-getFullBudget.js
@@ -1,10 +1,9 @@
-import getBudgetObject from './7-getBudgetObject.js';
+import getBudgetObject from './7-getBudgetObject';
 
 export default function getFullBudgetObject(income, gdp, capita) {
   const budget = getBudgetObject(income, gdp, capita);
   const fullBudget = {
     ...budget,
-    // Utilisation de la syntaxe des méthodes de propriété ES6
     getIncomeInDollars(income) {
       return `$${income}`;
     },
