c90dfdf06f79a6496be5370790192d18a08457ad
EL274 <tresormakosso510@gmail.com>
2024-11-15 23:58:40 +0100

    ok

--- ES6_classes/10-car.js
+++ ES6_classes/10-car.js
@@ -1,16 +1,11 @@
 export default class Car {
-    constructor(brand, motor, color) {
-      this._brand = brand;
-      this._motor = motor;
-      this._color = color;
-    }
-  
-    cloneCar() {
-      const current = this;
-      return Object.assign(Object.create(Object.getPrototypeOf(current)), {
-        _brand: undefined,
-        _motor: undefined,
-        _color: undefined,
-      });
-    }
+  constructor(brand, motor, color) {
+    this._brand = brand;
+    this._motor = motor;
+    this._color = color;
   }
+
+  cloneCar() {
+    return new this.constructor();
+  }
+}
