169610a43891a40156f458800caae7f05c2e4c7a
EL274 <tresormakosso510@gmail.com>
2024-11-15 16:36:39 +0100

    ok

--- ES6_basic/8-getBudgetCurrentYear.js
+++ ES6_basic/8-getBudgetCurrentYear.js
@@ -1,13 +1,14 @@
 function getCurrentYear() {
-    const date = new Date();
-    return date.getFullYear();
-  }
-  
-  export default function getBudgetForCurrentYear(income, gdp, capita) {
-    // Utilisation des noms de propriétés calculés directement dans la déclaration de l'objet
-    return {
-      [`income-${getCurrentYear()}`]: income,
-      [`gdp-${getCurrentYear()}`]: gdp,
-      [`capita-${getCurrentYear()}`]: capita
-    };
-  }
+  const date = new Date();
+  return date.getFullYear();
+}
+
+export default function getBudgetForCurrentYear(income, gdp, capita) {
+  const budget = {
+    [`income-${getCurrentYear()}`]: income,
+    [`gdp-${getCurrentYear()}`]: gdp,
+    [`capita-${getCurrentYear()}`]: capita,
+  };
+
+  return budget;
+}
