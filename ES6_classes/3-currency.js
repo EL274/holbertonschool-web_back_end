21832a0fb74f11968796ace43f1ee42a456f61d0
EL274 <tresormakosso510@gmail.com>
2024-11-16 00:15:53 +0100

    ok

--- ES6_classes/3-currency.js
+++ ES6_classes/3-currency.js
@@ -1,31 +1,26 @@
 export default class Currency {
-    constructor(code, name) {
-      if (typeof code !== 'string') throw TypeError('student must be a String');
-      if (typeof name !== 'string') throw TypeError('student must be a String');
-  
-      this._code = code;
-      this._name = name;
-    }
-  
-    get code() {
-      return this._code;
-    }
-  
-    get name() {
-      return this._name;
-    }
-  
-    set code(newCode) {
-      if (typeof code !== 'string') throw TypeError('student must be a String');
-      this._code = newCode;
-    }
-  
-    set name(newName) {
-      if (typeof newName !== 'string') throw TypeError('student must be a String');
-      this._name = newName;
-    }
-  
-    displayFullCurrency() {
-      return `${this._name} (${this._code})`;
-    }
+  constructor(code, name) {
+    this._code = code;
+    this._name = name;
   }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(value) {
+    this._name = value;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  set code(value) {
+    this._code = value;
+  }
+
+  displayFullCurrency() {
+    return `${this._name} (${this._code})`;
+  }
+}
