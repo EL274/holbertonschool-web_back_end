f776df3d1bbe2ede9bb694948f15afec3018c98f
EL274 <tresormakosso510@gmail.com>
2024-11-15 16:08:55 +0100

    ok

--- ES6_basic/4-rest-parameter.js
+++ ES6_basic/4-rest-parameter.js
@@ -1,10 +1,3 @@
-/**
- * Retourne le nombre d'arguments passés à la fonction.
- * 
- * @param {...*} args - Les arguments passés à la fonction (paramètre rest).
- * @return {number} Le nombre d'arguments passés.
- */
 export default function returnHowManyArguments(...args) {
-    // Retourne la longueur du tableau args, qui correspond au nombre d'arguments
     return args.length;
 }
