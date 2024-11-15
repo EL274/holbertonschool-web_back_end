fbb72ad4784467853c252d9968d92f030d527e3f
EL274 <tresormakosso510@gmail.com>
2024-11-15 13:27:52 +0100

    ok

--- ES6_basic/0-constants.js
+++ ES6_basic/0-constants.js
@@ -1,23 +1,15 @@
-// Fonction taskFirst utilisant 'const'
 export function taskFirst() {
-    // Utilisation de 'const' car la valeur de 'task' ne change pas
     const task = 'I prefer const when I can.';
     return task;
-  }
-  
-  // Fonction getLast reste inchangée
+}
+
   export function getLast() {
     return ' is okay';
-  }
-  
-  // Fonction taskNext utilisant 'let'
+  }   
+
   export function taskNext() {
-    // Utilisation de 'let' car 'combination' sera modifiée
     let combination = 'But sometimes let';
-    
-    // Modification de 'combination' en ajoutant le résultat de getLast()
     combination += getLast();
   
     return combination;
   }
-
