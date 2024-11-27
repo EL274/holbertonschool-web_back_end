663f27d54e40fc0cdb2844933d98150f2e521811
EL274 <tresormakosso510@gmail.com>
2024-11-27 03:50:41 +0100

    ok

--- ES6_data_manipulation/2-get_students_by_loc.js
+++ ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,3 +1,3 @@
-export default function getStudentsByLocation(list , city) {
-  return list.filter((x) => x.location === city);
+export default function getStudentsByLocation(student , city) {
+  return students.filter((student) => student.location === city);
 }
