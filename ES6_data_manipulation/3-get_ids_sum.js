a6bd09e8ee00d0c4c605bfd108f16356dd84767b
EL274 <tresormakosso510@gmail.com>
2024-11-27 05:04:28 +0100

    ok

--- ES6_data_manipulation/3-get_ids_sum.js
+++ ES6_data_manipulation/3-get_ids_sum.js
@@ -1,3 +1,3 @@
 export default function getStudentIdsSum(list) {
-    return list.reduce((accumulator, list) => accumulator + student.id, 0);
+    return list.reduce((accumulator, list) => accumulator + list.id, 0);
   }
