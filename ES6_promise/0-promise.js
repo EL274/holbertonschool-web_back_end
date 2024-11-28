09b4be127b94eac9ad8d77129126b19c705e529b
EL274 <tresormakosso510@gmail.com>
2024-11-28 12:44:17 +0100

    ok

--- ES6_promise/0-promise.js
+++ ES6_promise/0-promise.js
@@ -1,6 +1,7 @@
 export default function getResponseFromAPI() {
     return new Promise((resolve) => {
-        resolve(true);
-
+        setTimeout(()=> {resolve('true');
+            resolve('true');
+        });
     });
 }
