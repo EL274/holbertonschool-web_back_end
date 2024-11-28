05ee949a8e07f312a03ab00d2ea26a9b276a4802
EL274 <tresormakosso510@gmail.com>
2024-11-28 17:19:59 +0100

    ok

--- ES6_promise/9-try.js
+++ ES6_promise/9-try.js
@@ -1,14 +1,12 @@
 export default function guardrail(mathfunction) {
   const queue = [];
   try {
-    const (result) = mathfunction();
-    queue.push(result)
-  }
-  catch (error ){
+    const result = mathfunction();
+    queue.push(result);
+  } catch (error) {
     queue.push('Error: $(error.message)');
+  } finally {
+    queue.push('Guardrail was processed');
   }
-  finally{
-    queue.push ("Guardrail was processed");
-  }
-    return queue;
+  return queue;
 }
