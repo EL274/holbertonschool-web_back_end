085c61471b310ed8f86c09d30e97808320d27f17
tresor@foxkiradev274 <tresormakosso510@gmail.com>
2024-12-06 23:25:21 +0100

    ok

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,4 +1,9 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+"""function add with floats numbers"""
+
 
 def add(a: float, b: float) -> float:
+    """
+    Adds two floating-point numbers and returns the result.
+    """
     return a + b
