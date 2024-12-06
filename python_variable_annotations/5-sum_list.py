d3e12e6955bc58857e153406fe9caff18d350f00
tresor@foxkiradev274 <tresormakosso510@gmail.com>
2024-12-06 23:52:00 +0100

    ok

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -1,5 +1,9 @@
-#!/usr/bin/python3
-from typing import float 
+#!/usr/bin/env python3
+
+"""5-sum_list.py"""
+from typing import float
+
 
 def sum_list(imput_list: list[float]) -> float:
+    """list of floats to a sum of floats"""
     return sum(imput_list)
