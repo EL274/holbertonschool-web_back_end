1738709c3b64e3ceef85a7a2ba42421d378ad1c7
tresor@foxkiradev274 <tresormakosso510@gmail.com>
2024-12-06 23:55:09 +0100

    ok

--- python_variable_annotations/6-sum_mixed_list.py
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -1,5 +1,12 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 
-from typing import  List
-def sum_mixed_list(mxd_lst: list[float]) -> float:
-    return sum(mxd_lst)
+"""6-sum_mixed_list.py"""
+from typing import List, Union
+
+
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
+    """list of mixed int and floats to a float"""
+    res: float = 0
+    for i in mxd_lst:
+        res += i
+    return res
