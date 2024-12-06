c6e7457e52e4ed5cab35210e85a95efcc5c97282
tresor@foxkiradev274 <tresormakosso510@gmail.com>
2024-12-06 23:56:49 +0100

    ok

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -1,4 +1,9 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 
-def to_kv(k: str, v: float) ->float:
-    return v * v
+"""7-to_kv.py"""
+from typing import Union, Tuple
+
+
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+    """k and v to a tuple (k,v*v)"""
+    return (k, v * v)
