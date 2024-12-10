6859bf69256c18686aa84e0d4728baed5793515e
tresor@foxkiradev274 <tresormakosso510@gmail.com>
2024-12-10 14:13:51 +0100

    ok

--- pagination/1-simple_pagination.py
+++ pagination/1-simple_pagination.py
@@ -1,1 +1,15 @@
 #!/usr/bin/env python3
+
+
+def index_range(page, page_size):
+    start_index =(page - 1) * page_size
+    end_index = start_index + page_size 
+    return (start_index, end_index)
+
+class Pagination:
+    def __init__(self, page, page_size):
+        self.page = page 
+        self.page_size = page_size
+
+    def get_index_range(self):
+        return index_range(self.page, self.page_size)
