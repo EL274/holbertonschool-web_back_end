a68d0827dacf5c77cef4cbde4117e1eb3412fca3
tresor@foxkiradev274 <tresormakosso510@gmail.com>
2024-12-11 16:40:51 +0100

    ok

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -2,6 +2,7 @@
 
 
 def index_range(page, page_size):
+    """Calculate the start and end indices for pagination."""
     start_index = (page - 1) * page_size
     end_index = start_index + page_size
     return (start_index, end_index)
