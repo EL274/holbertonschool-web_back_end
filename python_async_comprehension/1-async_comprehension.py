add34e4fd933836a2901709fc3fb24c9f3e25cca
tresor@foxkiradev274 <tresormakosso510@gmail.com>
2024-12-11 02:26:35 +0100

    ok

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -2,12 +2,9 @@
 
 import asyncio
 from typing import List
-async_generator = __import__  ('0-async_generator.py').async_generator
+async_generator = __import__('0-async_generator.py').async_generator
+
 
 async def async_comprehension() -> List[float]:
-    """
-    Collecte 10 nombres aléatoires à l'aide d'une compréhension asynchrone sur async_generator.
-    
-    :return: Une liste contenant 10 nombres flottants aléatoires.
-    """
+    """Collecte 10 nombres aléatoires à l'aide d'une compréhension"""
     return [number async for number in async_generator()]
