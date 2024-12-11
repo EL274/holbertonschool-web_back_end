06ef04a665bb686e4d7ca884c2d78fc0403b028a
tresor@foxkiradev274 <tresormakosso510@gmail.com>
2024-12-12 00:31:34 +0100

    ok

--- pagination/2-hypermedia_pagination.py
+++ pagination/2-hypermedia_pagination.py
@@ -3,10 +3,12 @@
 
 import math
 
+
 def index_range(page, page_size):
     start_index = (page - 1) * page_size
     end_index = start_index + page_size
     return (start_index, end_index)
+
 
 class Pagination:
     def __init__(self, dataset, page, page_size):
@@ -22,7 +24,6 @@ class Pagination:
         data = self.get_page()
         total_items = len(self.dataset)
         total_pages = math.ceil(total_items / self.page_size)
-        
         next_page = self.page + 1 if self.page < total_pages else None
         prev_page = self.page - 1 if self.page > 1 else None
 
@@ -33,4 +34,4 @@ class Pagination:
             'next_page': next_page,
             'prev_page': prev_page,
             'total_pages': total_pages
-        } 
+        }
