5619507c9b9137411c85fe3b55f1c642fdb85bcd
tresor@foxkiradev274 <tresormakosso510@gmail.com>
2024-12-12 05:14:23 +0100

    ok

--- pagination/2-hypermedia_pagination.py
+++ pagination/2-hypermedia_pagination.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
-""" Simple pagination """
+""" Hypermedia pagination """
 import csv
-import math
-from typing import List, Tuple
+from math import ceil
+from typing import List, Tuple, Dict
 
 
 class Server:
@@ -42,6 +42,50 @@ class Server:
         pagination: List = self.dataset()
 
         return (pagination[range[0]:range[1]])
+
+    def get_hyper(self, page: int = 1, page_size: int = 10) -> Dict:
+        """
+            Range of the page
+
+            Args:
+                page: Current page
+                page_size: Total size of the page
+
+            Return:
+                Dict with different arguments
+                page_size: the length of the returned dataset page
+                page: the current page number
+                data: the dataset page
+                (equivalent to return from previous task)
+                next_page: number of the next page, None if no next page
+                prev_page: number of the previous page,
+                None if no previous page
+                total_pages: the total number of pages
+                in the dataset as an integer
+        """
+
+        data = []
+        try:
+            data = self.get_page(page, page_size)
+        except AssertionError:
+            return {}
+
+        dataset: List = self.dataset()
+        totalpag: int = len(dataset) if dataset else 0
+        totalpag = ceil(totalpag / page_size)
+        prevpag: int = (page - 1) if (page - 1) >= 1 else None
+        nextpag: int = (page + 1) if (page + 1) <= totalpag else None
+
+        hypermedia: Dict = {
+            'page_size': page_size,
+            'page': page,
+            'data': data,
+            'next_page': nextpag,
+            'prev_page': prevpag,
+            'total_pages': totalpag,
+        }
+
+        return hypermedia
 
 
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
