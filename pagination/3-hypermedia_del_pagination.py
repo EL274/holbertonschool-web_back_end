5ebfd5e41755865b8241c178477be7c53785884e
tresor@foxkiradev274 <tresormakosso510@gmail.com>
2024-12-12 10:51:37 +0100

    ok

--- pagination/3-hypermedia_del_pagination.py
+++ pagination/3-hypermedia_del_pagination.py
@@ -4,12 +4,16 @@ import math
 from typing import List, Tuple, Dict
 
 # Fonction index_range
+
+
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
     start_index = (page - 1) * page_size
     end_index = start_index + page_size
     return (start_index, end_index)
 
 # Classe Pagination
+
+
 class Pagination:
     def __init__(self, dataset: List, page: int, page_size: int):
         self.dataset = dataset
@@ -24,7 +28,6 @@ class Pagination:
         data = self.get_page()
         total_items = len(self.dataset)
         total_pages = math.ceil(total_items / self.page_size)
-        
         next_page = self.page + 1 if self.page < total_pages else None
         prev_page = self.page - 1 if self.page > 1 else None
 
@@ -36,4 +39,3 @@ class Pagination:
             'prev_page': prev_page,
             'total_pages': total_pages
         }
-
