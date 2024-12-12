a17097fdf33716421f76409a154c7b80e41d42ba
tresor@foxkiradev274 <tresormakosso510@gmail.com>
2024-12-12 10:38:11 +0100

    ok

--- pagination/1-simple_pagination.py
+++ pagination/1-simple_pagination.py
@@ -1,16 +1,60 @@
 #!/usr/bin/env python3
+""" Simple pagination """
+import csv
+import math
+from typing import List, Tuple
 
 
-def index_range(page, page_size):
-    start_index = (page - 1) * page_size
-    end_index = start_index + page_size
-    return (start_index, end_index)
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """
+            Get the page
+
+            Args:
+                page: Current page
+                page_size: Total size of the page
+
+            Return:
+                List of the pagination done
+        """
+        assert isinstance(page, int) and page > 0
+        assert isinstance(page_size, int) and page_size > 0
+
+        range: Tuple = index_range(page, page_size)
+        pagination: List = self.dataset()
+
+        return (pagination[range[0]:range[1]])
 
 
-class Pagination:
-    def __init__(self, page, page_size):
-        self.page = page
-        self.page_size = page_size
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """
+    Range of the page
+    Args:
+        page: Current page
+        page_size: Total size of the page
+    Return:
+        tuple with the range start and end size page
+    """
 
-    def get_index_range(self):
-        return index_range(self.page, self.page_size)
+    final_size: int = page * page_size
+    start_size: int = final_size - page_size
+
+    return (start_size, final_size)
