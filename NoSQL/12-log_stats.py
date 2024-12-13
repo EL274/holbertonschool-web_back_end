36288ad0995971743632301bc50141c99c09371d
tresor@foxkiradev274 <tresormakosso510@gmail.com>
2024-12-13 16:04:08 +0100

    ok

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -1,25 +1,31 @@
 #!/usr/bin/env python3
-"""This module contains a method use to implement mongodb"""
+
+"""
+12-log_stats.py
+"""
+
 from pymongo import MongoClient
 
 
-def display_information(mongo_collection):
-    """Provides some stats about Nginx logs"""
-    print("{} logs".format(mongo_collection.count_documents({})))
+def log_stats():
+    """
+    Provides stats about Nginx logs stored in MongoDB.
+    """
+    client = MongoClient('mongodb://localhost:27017/')
+    db = client.logs
+    collection = db.nginx
+    total = collection.count_documents({})
+    print(f"{total} logs")
     print("Methods:")
-
     methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
-
     for method in methods:
-        print("\tmethod {}: {}".format(
-            method, mongo_collection.count_documents({"method": method})))
-
-    print("{} status check".format(mongo_collection.count_documents(
-        {"method": methods[0], "path": "/status"})))
+        method_count = collection.count_documents({"method": method})
+        print(f"\tmethod {method}: {method_count}")
+    status = collection.count_documents(
+        {"method": "GET", "path": "/status"}
+        )
+    print(f"{status} status check")
 
 
 if __name__ == "__main__":
-    client = MongoClient('mongodb://127.0.0.1:27017')
-    nginx_collection = client.logs.nginx
-
-    display_information(nginx_collection)
+    log_stats()
