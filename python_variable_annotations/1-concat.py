b6088ffb9fec31724430d4c6edc7f0ba4c35b6ea
tresor@foxkiradev274 <tresormakosso510@gmail.com>
2025-04-23 16:18:34 +0200

    ok

--- python_variable_annotations/1-concat.py
+++ python_variable_annotations/1-concat.py
@@ -10,7 +10,6 @@ def concat(str1: str, str2: str) -> str:
     Args:
         str1: Première chaîne à concaténer
         str2: Deuxième chaîne à concaténer
-    
     Returns:
         La chaîne résultant de la concaténation de str1 et str2
     """
