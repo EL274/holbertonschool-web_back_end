df1daccd9f7c1165fa24f3b7e51bc85ef999e07a
tresor@foxkiradev274 <tresormakosso510@gmail.com>
2025-04-24 01:23:13 +0200

    ok

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""Module pour les opérations mathématiques de base
+Ce module fournit des fonctions mathématiques avec des annotations de type
+"""
+import math
+
+
+def floor(n: float) -> int:
+    """Retourne l'arrondi inférieur (floor) d'un nombre flottant.
+    
+    Args:
+        n: Nombre flottant à arrondir vers le bas
+    
+    Returns:
+        L'entier résultant de l'arrondi vers le bas (floor)
+    """
+    return math.floor(n)
