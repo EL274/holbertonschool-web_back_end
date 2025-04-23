ef45e84985560cffd165b79f567ba7d541275789
tresor@foxkiradev274 <tresormakosso510@gmail.com>
2025-04-23 16:17:06 +0200

    ok

--- python_variable_annotations/1-concat.py
+++ python_variable_annotations/1-concat.py
@@ -1,10 +1,18 @@
 #!/usr/bin/env python3
+"""Module pour la concaténation de chaînes de caractères.
+
+Ce module fournit une fonction simple pour concaténer deux chaînes.
+"""
+
+
 def concat(str1: str, str2: str) -> str:
     """Concatène deux chaînes de caractères et retourne le résultat.
+    
     Args:
         str1: Première chaîne à concaténer
         str2: Deuxième chaîne à concaténer
+    
     Returns:
-        La chaîne concaténée de str1 et str2
+        La chaîne résultant de la concaténation de str1 et str2
     """
     return str1 + str2
