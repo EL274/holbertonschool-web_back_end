be8b278f95558ecd4bf93116b870630101a7cf6b
tresor@foxkiradev274 <tresormakosso510@gmail.com>
2025-04-23 13:14:09 +0200

    ok

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,10 +1,17 @@
 #!/usr/bin/env python3
+"""Module pour une fonction d'addition flottants.
+
+Ce module fournit une fonction simple pour additionner deux nombres flottants.
+"""
+
 def add(a: float, b: float) -> float:
     """
-    Adds two floatters numbers and returns the sum
+    Additionnr deux nombres flottants et retourne leur somme.
+
     Args:
         a (float): the first number
         b (float): the second number
+        
     Returns:
         float: the sum of a and b
     """
