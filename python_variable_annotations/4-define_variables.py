8716b1c838a6bcffbd440db2e37c60e413bea536
tresor@foxkiradev274 <tresormakosso510@gmail.com>
2025-04-24 07:25:28 +0200

    ok

---
+++ python_variable_annotations/4-define_variables.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""Module pour démontrer les annotations de variables.
+
+Ce module montre comment annoter des variables avec différents types.
+"""
+
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: bool = True
+school: str = "Holberton"
