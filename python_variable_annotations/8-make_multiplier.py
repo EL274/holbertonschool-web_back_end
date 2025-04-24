2d023bf867010194c6e6573e77505be775ad6a70
tresor@foxkiradev274 <tresormakosso510@gmail.com>
2025-04-24 14:44:58 +0200

    ok

---
+++ python_variable_annotations/8-make_multiplier.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""Module pour créer un multiplicateur
+Ce module fournit la fonction pour créer un multiplicateur 
+"""
+from typing import Callable
+def make_multiplier (multiplier: float) -> Callable[[float], float]:
+    """Crée une fonction qui multiplie un nombre par un multiplicateur
+     Args:
+        multiplier: le mutiplicateur
+        Returns:
+            Une fonction qui multiplie un nombre par le multiplicateur
+            """
+    def multiplier_function (x: float) -> float:
+        return x * multiplier
+    return multiplier_function
