f8f6dd7d990a1b817d9703b8214e09d5b2882005
tresor@foxkiradev274 <tresormakosso510@gmail.com>
2025-04-29 10:28:38 +0200

    change comment

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -1,21 +1,23 @@
 #!/usr/bin/env python3
-""" Range simple helper fun """
+"""Gamme simple aide amusante"""
 from typing import Tuple
 
 
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
     """
-        Range of the page
+        Portée de la page
 
         Args:
             page: Current page
             page_size: Total size of the page
 
         Return:
-            tuple with the range start and end size page
+           tuple avec la plage de taille de début et de fin
+           de page
     """
 
     final_size: int = page * page_size
     start_size: int = final_size - page_size
 
     return (start_size, final_size)
+ 
